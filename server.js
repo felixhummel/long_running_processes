91241cc845b0f868f7a815fc10f75a7c5e649d83
Felix Hummel <felix@felixhummel.de>
2012-01-18 22:06:30 +0100

    works with socket.io

--- server.js
+++ server.js
@@ -1,49 +1,31 @@
-var http = require('http');
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+
 var child_process = require('child_process');
 var util = require('util');
 
-var logEvents = function(obj, name, events) {
-  var i;
-  for (i=0; i < events.length; i++) {
-    var ev = events[i];
-    obj.on(ev, function() {
-      var args = arguments.length == 0 ? '' : util.inspect(arguments);
-      console.log(name+': ' + ev + ' ' + args);
+app.listen(1337);
+
+function handler(req, res) {
+  fs.readFile(__dirname + '/index.html', function (err, data) {
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function(socket) {
+  var proc;
+  socket.on('run', function(data) {
+    proc = child_process.spawn('./example_process');
+    proc.stdout.setEncoding('utf8');
+    proc.stdout.on('data', function(data) {
+      socket.emit('proc', data);
+      util.inspect(data);
     });
-  }
-};
-var logConnectionEvents = function(conn, name) {
-  logEvents(conn, name, ['connect', 'data', 'end', 'timeout', 'drain', 'error', 'close']);
-};
-var logStreamEvents = function(stream, name) {
-  logEvents(stream, name, ['data', 'drain', 'error', 'close', 'pipe']);
-};
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {
-    'transfer-encoding': 'chunked'
   });
-  logStreamEvents(req, 'Request Stream');
-  logStreamEvents(res, 'Response Stream');
-  logConnectionEvents(res.connection, 'Request Connection');
-  logConnectionEvents(res.connection, 'Response Connection');
-
-  proc = child_process.spawn('./example_process');
-
-  logStreamEvents(proc.stdout, 'STDOUT');
-  util.pump(proc.stdout, res);
-  /*
-  proc.stdout.on('data', function(data) {
-    res.write(data);
+  socket.on('kill', function(data) {
+    proc.kill()
   });
-  */
-  req.connection.on('end', function() {
-    res.end();
-    proc.kill();
-    console.log('=============================================================');
-  });
-
-
-}).listen(1337, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+});
 
